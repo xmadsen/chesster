5d95c233ab796ea3550737e2d14f9b66dceccda9
xmadsen <xander.madsen@gmail.com>
2020-07-03 21:50:29 -0400

    Add Pawn class init with 1 point value

--- chess/piece.py
+++ chess/piece.py
@@ -2,6 +2,7 @@ class Piece():
 
     location = ()
     color = None
+    point_value = 0
 
     def __init__(self, location):
         self.is_alive = True
@@ -13,3 +14,10 @@ class Piece():
 
     def get_rank(self):
         return self.location[1]
+
+
+class Pawn(Piece):
+
+    def __init__(self, location):
+        super().__init__(location)
+        self.point_value = 1
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -1,22 +1,32 @@
 import pytest
-from chess.piece import Piece
+from random import randrange
+from chess.piece import Piece, Pawn
 
 
 def test_new_piece_starts_alive():
-    test_piece = Piece(location=(0, 0))
+    test_piece = Piece((0, 0))
     assert test_piece.is_alive
 
 
 def test_new_piece_has_provided_location():
-    test_piece = Piece((0, 0))
-    assert test_piece.location == (0, 0)
+    location = (randrange(8), randrange(8))
+    test_piece = Piece(location)
+    assert test_piece.location == location
 
 
 def test_get_file_returns_correct_value():
-    test_piece = Piece((0, 1))
-    assert test_piece.get_file() == 0
+    location = (randrange(8), randrange(8))
+    test_piece = Piece(location)
+    assert test_piece.get_file() == location[0]
 
 
 def test_get_rank_returns_correct_value():
-    test_piece = Piece((0, 1))
-    assert test_piece.get_rank() == 1
+    location = (randrange(8), randrange(8))
+    test_piece = Piece(location)
+    assert test_piece.get_rank() == location[1]
+
+
+def test_pawn_has_point_value_of_1():
+    location = (randrange(8), randrange(8))
+    test_pawn = Pawn(location)
+    assert test_pawn.point_value == 1
