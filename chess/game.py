573af514d0f35ce7d6fa2d6018c5b7a06be12d79
xmadsen <xander.madsen@gmail.com>
2020-07-06 14:21:31 -0400

    Add pPlayer class and update Game class

--- chess/game.py
+++ chess/game.py
@@ -1,7 +1,8 @@
 from ui import UserInterface
 from cli import CliRenderer
-
+from constants import PlayerConstants as pc
 from board import Board
+from player import Player
 
 
 class Game():
@@ -9,3 +10,7 @@ class Game():
     def __init__(self):
         self.board = Board()
         self.cli = CliRenderer(self.board)
+
+        black_player = Player(pc.BLACK_PLAYER)
+        white_player = Player(pc.WHITE_PLAYER)
+        self.players = [white_player, black_player]
