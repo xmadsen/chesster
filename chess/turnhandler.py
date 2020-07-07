4f8d0335bbbaf8c1aac3840802c014383c86f8b8
xmadsen <xander.madsen@gmail.com>
2020-07-06 20:46:19 -0400

    Update turnhandler to use its own location as where the cursor is, and corresponding tests for directional input

--- chess/turnhandler.py
+++ chess/turnhandler.py
@@ -5,36 +5,45 @@ from constants import InputConstants as ic
 class TurnHandler():
     def __init__(self, board):
         self.board = board
+        self.location = (0, 0)
 
-    def process_input(self, input_key, location):
-        x = location[0]
-        y = location[1]
+    def process_input(self, input_key):
+        x = self.location[0]
+        y = self.location[1]
         if input_key == ' ':
             self.select(x, y)
         if input_key == ic.RIGHT_KEY:
-            if location[0] + 1 <= 7:
+            if x + 1 <= 7:
                 self.board.squares[x][y].is_hovered = False
                 self.board.squares[x + 1][y].is_hovered = True
+                self.location = (x + 1, y)
         elif input_key == ic.LEFT_KEY:
-            if location[0] - 1 >= 0:
+            if x - 1 >= 0:
                 self.board.squares[x][y].is_hovered = False
                 self.board.squares[x - 1][y].is_hovered = True
+                self.location = (x - 1, y)
         elif input_key == ic.UP_KEY:
-            if location[1] + 1 <= 7:
+            if y + 1 <= 7:
                 self.board.squares[x][y].is_hovered = False
                 self.board.squares[x][y + 1].is_hovered = True
+                self.location = (x, y + 1)
         elif input_key == ic.DOWN_KEY:
-            if location[1] - 1 >= 0:
+            if y - 1 >= 0:
                 self.board.squares[x][y].is_hovered = False
                 self.board.squares[x][y - 1].is_hovered = True
+                self.location = (x, y - 1)
 
-    def select(self, x, y):
+    def select(self, location):
+        x = location[0]
+        y = location[1]
         piece = self.board.squares[x][y]
 
         if isinstance(piece, Piece):
             piece.select()
 
-    def deselect(self, x, y):
+    def deselect(self, location):
+        x = location[0]
+        y = location[1]
         piece = self.board.squares[x][y]
 
         if isinstance(piece, Piece):
--- tests/test_turnhandler.py
+++ tests/test_turnhandler.py
@@ -19,60 +19,52 @@ def handler(board):
 
 def test_turnhandler_correctly_selects_piece_in_starting_hover_position(handler):
     piece_location = (0, 0)
-    x = piece_location[0]
-    y = piece_location[1]
-    piece = handler.board.squares[x][y]
+    piece = handler.board.get_piece_at_location(piece_location)
 
     assert not piece.is_selected
     assert piece.is_hovered
 
-    handler.select(x, y)
+    handler.select(piece_location)
     assert piece.is_selected
     assert not piece.is_hovered
 
 
 def test_turnhandler_correctly_deselects_selected_piece(handler):
     piece_location = (0, 0)
-    x = piece_location[0]
-    y = piece_location[1]
-    piece = handler.board.squares[x][y]
+    piece = handler.board.get_piece_at_location(piece_location)
 
-    handler.select(x, y)
+    handler.select(piece_location)
     assert piece.is_selected
     assert not piece.is_hovered
 
-    handler.deselect(x, y)
+    handler.deselect(piece_location)
     assert not piece.is_selected
     assert piece.is_hovered
 
 
 def test_turnhandler_selects_nothing_in_blank_square(handler):
     piece_location = (2, 2)  # no piece here at start of game
-    x = piece_location[0]
-    y = piece_location[1]
-    piece = handler.board.squares[x][y]
+    piece = handler.board.get_piece_at_location(piece_location)
+
     assert not isinstance(piece, Piece)
 
-    handler.select(x, y)
+    handler.select(piece_location)
     assert not isinstance(piece, Piece)
 
 
 def test_turnhandler_hovers_right_piece_on_right_key_input_from_left_wall(handler):
     starting_piece_location = (0, 0)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
 
     assert starting_piece.is_hovered
 
     right_piece_location = (1, 0)
-    rightx = right_piece_location[0]
-    righty = right_piece_location[1]
-    right_piece = handler.board.squares[rightx][righty]
+    right_piece = handler.board.get_piece_at_location(right_piece_location)
 
     assert not right_piece.is_hovered
 
-    handler.process_input(ic.RIGHT_KEY, starting_piece_location)
+    handler.process_input(ic.RIGHT_KEY)
 
     assert not starting_piece.is_hovered
     assert right_piece.is_hovered
@@ -80,35 +72,32 @@ def test_turnhandler_hovers_right_piece_on_right_key_input_from_left_wall(handle
 
 def test_turnhandler_does_nothing_on_right_key_input_from_right_wall(handler):
     starting_piece_location = (7, 0)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
-    handler.process_input(ic.RIGHT_KEY, starting_piece_location)
+    handler.process_input(ic.RIGHT_KEY)
 
     assert starting_piece.is_hovered
 
 
 def test_turnhandler_hovers_left_piece_on_left_key_input_from_right_wall(handler):
     starting_piece_location = (7, 0)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
     left_piece_location = (6, 0)
-    leftx = left_piece_location[0]
-    lefty = left_piece_location[1]
-    left_piece = handler.board.squares[leftx][lefty]
+    left_piece = handler.board.get_piece_at_location(left_piece_location)
 
     assert not left_piece.is_hovered
 
-    handler.process_input(ic.LEFT_KEY, starting_piece_location)
+    handler.location = starting_piece_location
+    handler.process_input(ic.LEFT_KEY)
 
     assert not starting_piece.is_hovered
     assert left_piece.is_hovered
@@ -116,35 +105,33 @@ def test_turnhandler_hovers_left_piece_on_left_key_input_from_right_wall(handler
 
 def test_turnhandler_does_nothing_on_left_key_input_from_left_wall(handler):
     starting_piece_location = (0, 6)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
-    handler.process_input(ic.LEFT_KEY, starting_piece_location)
+    handler.location = starting_piece_location
+    handler.process_input(ic.LEFT_KEY)
 
     assert starting_piece.is_hovered
 
 
 def test_turnhandler_hovers_up_piece_on_up_key_input_from_bottom_wall(handler):
     starting_piece_location = (5, 0)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
     up_piece_location = (5, 1)
-    upx = up_piece_location[0]
-    upy = up_piece_location[1]
-    up_piece = handler.board.squares[upx][upy]
+    up_piece = handler.board.get_piece_at_location(up_piece_location)
 
     assert not up_piece.is_hovered
 
-    handler.process_input(ic.UP_KEY, starting_piece_location)
+    handler.location = starting_piece_location
+    handler.process_input(ic.UP_KEY)
 
     assert not starting_piece.is_hovered
     assert up_piece.is_hovered
@@ -152,35 +139,33 @@ def test_turnhandler_hovers_up_piece_on_up_key_input_from_bottom_wall(handler):
 
 def test_turnhandler_does_nothing_on_up_key_input_from_top_wall(handler):
     starting_piece_location = (5, 7)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
-    handler.process_input(ic.UP_KEY, starting_piece_location)
+    handler.location = starting_piece_location
+    handler.process_input(ic.UP_KEY)
 
     assert starting_piece.is_hovered
 
 
 def test_turnhandler_hovers_down_piece_on_down_key_input_from_top_wall(handler):
     starting_piece_location = (3, 7)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
     down_piece_location = (3, 6)
-    downx = down_piece_location[0]
-    downy = down_piece_location[1]
-    down_piece = handler.board.squares[downx][downy]
+    down_piece = handler.board.get_piece_at_location(down_piece_location)
 
     assert not down_piece.is_hovered
 
-    handler.process_input(ic.DOWN_KEY, starting_piece_location)
+    handler.location = starting_piece_location
+    handler.process_input(ic.DOWN_KEY)
 
     assert not starting_piece.is_hovered
     assert down_piece.is_hovered
@@ -188,13 +173,13 @@ def test_turnhandler_hovers_down_piece_on_down_key_input_from_top_wall(handler):
 
 def test_turnhandler_does_nothing_on_down_key_input_from_bottom_wall(handler):
     starting_piece_location = (2, 0)
-    startx = starting_piece_location[0]
-    starty = starting_piece_location[1]
-    starting_piece = handler.board.squares[startx][starty]
+    starting_piece = handler.board.get_piece_at_location(
+        starting_piece_location)
     starting_piece.is_hovered = True
 
     assert starting_piece.is_hovered
 
-    handler.process_input(ic.DOWN_KEY, starting_piece_location)
+    handler.location = starting_piece_location
+    handler.process_input(ic.DOWN_KEY)
 
     assert starting_piece.is_hovered
